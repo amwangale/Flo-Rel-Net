0334d5d57602fe642d7b88d121d36674fc9a6922
Samuel Collet <samuelpcollet@gmail.com>
2019-07-12 01:48:17 -0500

    float conversion

--- SatelliteNode/Send/includes/send.h
+++ SatelliteNode/Send/includes/send.h
@@ -48,6 +48,37 @@ typedef struct s_node {
 	t_sender sender;
 } t_node;
 
+typedef struct s_float15 {
+	float sign: 1;
+	float value: 20;
+} float15;
+
 t_node new_node(char **argv) {
 	//
+}
+
+float15 float_to_float15(float num) {
+	if (num > pow(2, 10)) return (null);
+
+	int i;
+	float15 f;
+	double value;
+	double fraction;
+	double integral;
+
+	value = 0;
+	f = new_float15();
+	fraction = modf(num, &integral);
+	f.sign = (num >> 32) & 0x1;
+	f.value |= integral;
+	f.value <<= 10; // half-size
+
+	i = -1;
+	while (num > 0x3FF) {
+		num /= 10;
+	}
+
+	f.value |= num;
+
+	return (f);
 }
